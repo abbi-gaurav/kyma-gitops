9b42c9504279281b673ae054362783df1623e114
Gaurav Abbi <abbi.gaurav@gmail.com>
2019-12-06 15:11:45 +0100

    Create lambda and expose it

--- scripts/create-api.sh
+++ scripts/create-api.sh
@@ -2,15 +2,16 @@
 
 API_FILE=$1
 NAME=$2
+BRANCH=$3
 
 cat <<EOF > "${API_FILE}"
 apiVersion: gateway.kyma-project.io/v1alpha2
 kind: Api
 metadata:
-  name: ${NAME}
+  name: ${NAME}-${BRANCH}
 spec:
   authentication: []
-  hostname: ${NAME}
+  hostname: ${NAME}-${BRANCH}
   service:
     name: ${NAME}
     port: 8080
